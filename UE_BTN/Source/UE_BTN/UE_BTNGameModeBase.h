b643b2f1a4f39fe6eb16fac40723b0fe50d6d7c5
Agniv Chakraborty <agnivchakraborty@gmail.com>
2018-09-10 23:31:12 +0530

    UE_BT01 Creation of Project

---
+++ UE_BTN/Source/UE_BTN/UE_BTNGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_BTNGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_BTN_API AUE_BTNGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
